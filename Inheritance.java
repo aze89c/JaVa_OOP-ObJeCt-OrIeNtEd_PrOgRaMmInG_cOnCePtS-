ba8ed460aed983bebd57c44612b345a490b0a7c7
aze89c <azizbharmal07@gmail.com>
2024-11-27 21:36:01 +0530

    Update Inheritance.java

--- Inheritance.java
+++ Inheritance.java
@@ -46,7 +46,7 @@ return width * height * depth;
 // Here, Box is extended to include weight.
 class BoxWeight extends Box {
 double weight; // weight of box
-// constructor for BoxWeight
+//constructor for BoxWeight
 BoxWeight(double w, double h, double d, double m) {
 width = w;
 height = h;
